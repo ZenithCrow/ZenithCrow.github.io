b9c1790b43fa6ac4f038c0a0a5dc0758a4f1bcd0
ZenithMeetsNadir <martin-vlcek@post.cz>
2026-01-29 21:49:28 +0100

    responsive ascii art

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let dotState = 3;
 
-document.querySelectorAll('p').forEach(p => {
-    if (p.textContent.endsWith('...')) {
-        setInterval(() => {
-            dotState = dotState % 3 + 1;
-            p.textContent = p.textContent.replace(/\.*$/, '.'.repeat(dotState));
-        }, 600);
-    }
+document.querySelectorAll('.three-dots').forEach(s => {
+    setInterval(() => {
+        dotState = dotState % 3 + 1;
+        s.textContent = s.textContent.replace(/\.*$/, '.'.repeat(dotState));
+    }, 600);
 });
+
+const defaultFontSize = parseFloat(getComputedStyle(document.getElementById('cro')).fontSize);
+
+function fitAsciiArt() {
+    const cro = document.getElementById('cro');
+    const croWidth = cro.getBoundingClientRect().width;
+    const parentWidth = cro.parentElement.getBoundingClientRect().width;
+
+    const curFontSize = parseFloat(getComputedStyle(cro).fontSize);
+
+    if (croWidth > parentWidth || curFontSize < defaultFontSize) {
+        let newFontSize = curFontSize * (parentWidth / croWidth);
+        if (newFontSize > defaultFontSize)
+            newFontSize = defaultFontSize;
+
+        document.querySelectorAll('.ascii-art').forEach(el => {
+            el.style.fontSize = `${newFontSize}px`;
+        });
+    }
+}
+
+fitAsciiArt();
+
+window.addEventListener('resize', fitAsciiArt);
